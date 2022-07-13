5fccae022d913dc1dac1201a964e3d8de4c70d4c
Elliot Long <e@elroid.com>
2022-07-13 15:53:28 +0100

    Adds bottom sheet

--- app/src/main/java/com/dentreality/spacekit/sample/SpaceKitViewFragment.kt
+++ app/src/main/java/com/dentreality/spacekit/sample/SpaceKitViewFragment.kt
@@ -1,16 +1,17 @@
 package com.dentreality.spacekit.sample
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.dentreality.spacekit.SpaceKitUIFragment
 import com.dentreality.spacekit.ext.Destination
 import com.dentreality.spacekit.ext.ListListener
 import com.dentreality.spacekit.sample.databinding.FragmentSpaceKitViewBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior.*
 
 /**
  * This fragment shows the SpaceKit UI along with a shopping list as bottom sheet
@@ -48,16 +49,43 @@ class SpaceKitViewFragment : Fragment() {
             spaceKitUiFragment.isLocationOverrideEnabled = true
 
             shoppingBasketFab.setOnClickListener {
-                Toast.makeText(requireContext(), "Shopping basket pressed", Toast.LENGTH_SHORT)
-                    .show()
+                toggleShoppingList()
             }
+
+            val sheetBehavior = from(listInclude.shoppingListSheet)
+            sheetBehavior.addBottomSheetCallback(object : BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    when (newState) {
+                        STATE_EXPANDED ->
+                            Log.i(TAG, "Shopping list drawer is open")
+                        STATE_COLLAPSED, STATE_HIDDEN ->
+                            Log.i(TAG, "Shopping list drawer is closed")
+                        else -> //other possibilities: STATE_DRAGGING, STATE_SETTLING, STATE_HALF_EXPANDED
+                            Log.v(TAG, "Shopping list drawer state:$newState")
+                    }
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+            })
+
         }
         return binding!!.root
     }
 
+    private fun toggleShoppingList() {
+        binding?.apply {
+            val sheetBehavior = from(listInclude.shoppingListSheet)
+            val newState = if (sheetBehavior.state != STATE_EXPANDED) {
+                STATE_EXPANDED
+            } else {
+                STATE_COLLAPSED
+            }
+            sheetBehavior.setState(newState)
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         viewModel.loadData()
     }
 
