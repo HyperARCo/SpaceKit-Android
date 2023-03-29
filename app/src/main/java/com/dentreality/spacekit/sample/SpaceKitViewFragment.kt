0a3181398e26d1eb839423379ea1d72cadded77c
Wojciech Debicki <wojciech.debicki@gmail.com>
2023-03-29 08:49:43 +0200

    SC-6008: SDK updated to latest 0.8.1 version

--- app/src/main/java/com/dentreality/spacekit/sample/SpaceKitViewFragment.kt
+++ app/src/main/java/com/dentreality/spacekit/sample/SpaceKitViewFragment.kt
@@ -2,10 +2,8 @@ package com.dentreality.spacekit.sample
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.dentreality.spacekit.SpaceKitUIFragment
 import com.dentreality.spacekit.ext.Destination
 import com.dentreality.spacekit.ext.ListListener
@@ -13,8 +11,6 @@ import com.dentreality.spacekit.ext.SpaceKitContextFactory
 import com.dentreality.spacekit.ext.SpaceKitVenue
 import com.dentreality.spacekit.sample.common.CachedAssetFile
 import com.dentreality.spacekit.sample.databinding.FragmentSpaceKitViewBinding
-import com.dentreality.spacekit.sample.databinding.ViewFooterShoppingListInfoBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior.*
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,12 +20,8 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class SpaceKitViewFragment : Fragment(R.layout.fragment_space_kit_view) {
-    private val binding by viewBinding(FragmentSpaceKitViewBinding::bind)
-
     private val viewModel: SpaceKitViewModel by viewModels()
     private lateinit var spaceKitUiFragment: SpaceKitUIFragment
-    private lateinit var listAdapter: ShoppingListAdapter
-    private var destinations: MutableList<Destination> = mutableListOf()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -47,70 +39,26 @@ class SpaceKitViewFragment : Fragment(R.layout.fragment_space_kit_view) {
 
         val spaceKitContext = SpaceKitContextFactory.create(spaceKitVenue)
 
+        /*
+        Custom UI injection:
+            spaceKitUiFragment.changeLevelSwitcher(levelSwitcherConfig: LevelSwitcherConfig)
+            spaceKitUiFragment.changeRecenterButton(recenterButtonConfig: RecenterButtonConfig)
+            spaceKitUiFragment.changeInfoView(infoViewConfig: InfoViewConfig)
+         */
+
         //load data
         spaceKitUiFragment.initialise(spaceKitContext)
-
-        val footerBinding = ViewFooterShoppingListInfoBinding.inflate(layoutInflater)
 
         //add listener for ordered items
         spaceKitContext.addListListener(object : ListListener<Destination> {
             override fun onUpdateOrderedDestinations(destinations: List<Destination>) {
-                this@SpaceKitViewFragment.destinations = destinations.toMutableList()
-                viewModel.onItemsOrdered(destinations.map { it as Product })
 
-                footerBinding.handleListOfProductUpdate(destinations.size)
-
-                if (destinations.isNotEmpty()) {
-                    footerBinding.handleTargetDestinationChanged(destinations.first() as Product)
-                } else {
-                    footerBinding.nextItemCard.isVisible = false
-                }
             }
         })
 
-        //create shopping list info footer
-        footerBinding.apply {
-            listOfProductsTV.text =
-                resources.getQuantityString(R.plurals.list_of_products, 0, 0)
-
-            listOfProductsSection.setOnClickListener {
-                toggleShoppingList()
-            }
-
-            //notify SpaceKitFragment that user found the product
-            gotItButton.setOnClickListener {
-                if (destinations.isEmpty()) return@setOnClickListener
-                destinations.removeFirst()
-                spaceKitContext.setDestinations(destinations)
-            }
-        }
-
-        //add footer into SpaceKitFragment
-        spaceKitUiFragment.addFooter(footerBinding.root)
-
-        listAdapter = ShoppingListAdapter()
-        listAdapter.onListClick = { viewModel.onListItemClicked(it) }
-        listAdapter.onPoolClick = { viewModel.onPoolItemClicked(it) }
-        binding.listInclude.shoppingListRecyclerView.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            adapter = listAdapter
-        }
-
-        viewModel.listPool.observe(requireActivity()) { listPool ->
-            listAdapter.updateList(listPool)
-            spaceKitContext.setDestinations(listPool.list)
-        }
-    }
-
-    private fun toggleShoppingList() {
-        binding.apply {
-            val sheetBehavior = from(listInclude.shoppingListSheet)
-            val newState = if (sheetBehavior.state != STATE_EXPANDED) {
-                STATE_EXPANDED
-            } else {
-                STATE_COLLAPSED
-            }
-            sheetBehavior.setState(newState)
-        }
+        /*
+        Changing destinations:
+            spaceKitContext.setDestinations(destinations: List<Destination>)
+        */
     }
 }
--- app/src/main/java/com/dentreality/spacekit/sample/SpaceKitViewModel.kt
+++ app/src/main/java/com/dentreality/spacekit/sample/SpaceKitViewModel.kt
@@ -1,7 +1,6 @@
 package com.dentreality.spacekit.sample
 
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,34 +9,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SpaceKitViewModel @Inject constructor(
-    private val shoppingList: ShoppingList
 ) : ViewModel() {
 
     companion object {
         private const val TAG = "SpaceKitViewModel"
     }
 
-    val listPool: MutableLiveData<ListPool> get() = shoppingList.poolListLiveData
-
     fun loadData() {
         Log.v(TAG, "loadData()")
         viewModelScope.launch {
-            Log.i(TAG, "original list:$shoppingList")
         }
-    }
-
-    fun onItemsOrdered(list: List<Product>) {
-        Log.v(TAG, "onItemsOrdered($list)")
-        shoppingList.setOrderedShoppingList(list)
-    }
-
-    fun onListItemClicked(product: Product) {
-        Log.v(TAG, "onListItemClicked(product:$product)")
-        shoppingList.removeFromShoppingList(product)
-    }
-
-    fun onPoolItemClicked(product: Product) {
-        Log.v(TAG, "onPoolItemClicked(product:$product)")
-        shoppingList.addToShoppingList(product)
     }
 }
